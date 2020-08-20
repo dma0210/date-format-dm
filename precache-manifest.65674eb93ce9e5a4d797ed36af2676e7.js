1b0ad3d91c1c86143a1380a8113dd2fa5e4acef0
Dma0210 <easyhtmlandcss@gmail.com>
2020-08-20 15:16:52 +0300

    Updates

---
+++ precache-manifest.65674eb93ce9e5a4d797ed36af2676e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcd801e90c84c8e66eb62c89ad264c79",
+    "url": "/date-format-dm/index.html"
+  },
+  {
+    "revision": "aa6e9d0f458317f49451",
+    "url": "/date-format-dm/static/css/main.4d6a81c5.chunk.css"
+  },
+  {
+    "revision": "1b7253561db683ca2969",
+    "url": "/date-format-dm/static/js/2.9367f94b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/date-format-dm/static/js/2.9367f94b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa6e9d0f458317f49451",
+    "url": "/date-format-dm/static/js/main.b93240b7.chunk.js"
+  },
+  {
+    "revision": "1caa10819f0f3c56179a",
+    "url": "/date-format-dm/static/js/runtime-main.4b1ca8e7.js"
+  }
+]);
